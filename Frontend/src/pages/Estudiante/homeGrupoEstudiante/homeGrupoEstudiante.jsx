44377b909eaa4cf39e8094afa0a5aec17fd9bffd
jHOkE <jhonumss@gmail.com>
2024-12-12 21:05:29 -0400

    listaGruposDeEstudiantescompletado

--- Frontend/src/pages/Estudiante/homeGrupoEstudiante/homeGrupoEstudiante.jsx
+++ Frontend/src/pages/Estudiante/homeGrupoEstudiante/homeGrupoEstudiante.jsx
@@ -3,7 +3,7 @@ const apiHost = import.meta.env.VITE_HOST;
 import Footer from "../../../components/baseUI/Footer/footer.jsx";
 import Header from "../../../components/baseUI/Header/header.jsx";
 import { styled } from "@mui/material/styles";
-import { Box, Typography} from "@mui/material";
+import { Box, Typography, Button} from "@mui/material";
 import { Fragment, useEffect, useState} from "react";
 import CardProgreso from '../../../components/cardsHome/cardEstudiante/cardProgreso.jsx'
 import CardPlanificacion from '../../../components/cardsHome/cardEstudiante/cardPlanificacion.jsx'
@@ -19,7 +19,7 @@ function HomeEstudiante() {
   const idGrupo = localStorage.getItem("idGrupo")
   useEffect(()=>{
     const getOrigin = async() =>{
-      const link = `${apiHost}/estudiante/getDataEstudiante`+new URLSearchParams({idGrupo})
+      const link = `${apiHost}/estudiante/getDataEstudiante/${idGrupo}`; 
       const body = {
         method: "GET",
         headers: {"Content-Type": "application/json"},
@@ -90,7 +90,10 @@ function HomeEstudiante() {
   return (
     <Fragment>
       <Header />
-      <Title variant="h5" sx={{marginTop:'5rem', textAlign:'center'}}>Bienvenid@, {nombreCompleto}</Title>
+      <Button variant="contained" color="primary" onClick={()=>navigate("/")} sx={{ marginTop:'5rem', marginLeft:'calc(5vw + 0.5rem)' }}>
+        Volver a la lista de grupos
+      </Button>
+      <Title variant="h5" sx={{ textAlign:'center'}}>Bienvenid@, {nombreCompleto}</Title>
       <Title variant="h6" sx={{textAlign:'center'}}>{localStorage.getItem('gestion')}</Title>
       <Container>
         {(aceptada!==0)?<CardProgreso></CardProgreso>:<></>} 
